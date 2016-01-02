606a9528b12984d5ba4d9d75015205d57410b040
tarun <tarun.baloch@autoninja.in>
2016-01-02 19:10:03 +0530

    classes & demo examples

--- classes.php
+++ classes.php
@@ -1,23 +1,43 @@
-<? php 
-	class Books{
-		public var $title;
-		public var $price;
-		var $pages;
+<?php 
+	class Books {
+      /* Member variables */
+      var $price;
+      var $title;
+      
+      /* Member functions */
+      function setPrice($par){
+         $this->price = $par;
+      }
+      
+      function getPrice(){
+         echo $this->price ."<br/>";
+      }
+      
+      function setTitle($par){
+         $this->title = $par;
+      }
+      
+      function getTitle(){
+         echo $this->title ." <br/>";
+      }
+   }
 
-		public function setTitle($name)
-		{
-			$this->title = $name;
-		}
+   	$physics = new Books;
+	$maths = new Books;
+	$chemistry = new Books;
 
-		public function setPrice($money){
-			$this->price = $money;
-		}
+	$physics->setTitle( "Physics for High School" );
+	$chemistry->setTitle( "Advanced Chemistry" );
+	$maths->setTitle( "Algebra" );
 
-		public function getTitle()
-		{
-			return $this->title;
-		}
-	}
+	$physics->setPrice( 10 );
+	$chemistry->setPrice( 15 );
+	$maths->setPrice( 7 );
 
-	
+	$physics->getTitle();
+	$chemistry->getTitle();
+	$maths->getTitle();
+	$physics->getPrice();
+	$chemistry->getPrice();
+	$maths->getPrice();
 ?>
